85392b19786ac15f19f5b587e56b152adcfe0df5
damiann024 <damian024@gmail.com>
2017-03-17 00:54:13 +0100

    config change

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,6 @@ module Mieszkania
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-    config.exceptions_app = self.routes
+    # -- all .rb files in that directory are automatically loaded
   end
 end
