707c5e1b03984aaf81f9c7fc8a998eaf97a7587f
damiann024 <damian024@gmail.com>
2017-03-17 01:10:08 +0100

    final build

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', registration: 'register', sign_up: 'cmon_let_me_in' }
   root to: "welcome#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "*path", to: redirect('/')
 end
